e4e6df78bd5c5c66c1908b0491b6a68d4d51fb41
Oren Damien Shomrai <damien.shomrai@pm.me>
2024-12-28 21:44:29 +0200

    chore(04): var typing and std streams

---
+++ 04-/main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+// run from shell: go run . < nums.txt
+func main() {
+
+	// initializes to "zero" by default
+	var sum float64
+	var n int
+
+	for {
+		var val float64
+
+		if _, err := fmt.Fscanln(os.Stdin, &val); err != nil {
+			break
+		}
+
+		sum += val
+		n++
+	}
+
+	if n == 0 {
+		fmt.Fprintln(os.Stderr, "no values")
+		os.Exit(-1)
+	}
+
+	fmt.Println("The average is", sum/float64(n))
+}
