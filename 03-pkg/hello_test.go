b8c692bf265cab3c059664afef9fe918d52be492
Oren Damien Shomrai <damien.shomrai@pm.me>
2024-12-28 20:39:11 +0200

    chore: added subtests

--- 03-pkg/hello_test.go
+++ 03-pkg/hello_test.go
@@ -2,7 +2,7 @@ package hello
 
 import "testing"
 
-func TestSayHello(t *testing.T) {
+func TestSayV1Hello(t *testing.T) {
 	want:= "Hello, test!"
 	got := Say("test")
 
@@ -12,10 +12,32 @@ func TestSayHello(t *testing.T) {
 }
 
 func TestSayV2Hello(t *testing.T) {
-	want:= "Hello, test!"
+	want := "Hello, test!"
 	got := SayV2([]string{"test"})
 
 	if want != got {
 		t.Errorf("wanted %s, got %s", want, got)
+	}
+}
+
+func TestSayV2HelloCases(t *testing.T) {
+	subtests := []struct {
+		items  []string
+		result string
+	}{
+		{
+			result: "Hello, world!",
+		},
+		{
+			items: []string{"Damien"},
+			result: "Hello, Damien!",
+		},
+	}
+
+	for _, st := range subtests {
+		if s := SayV2(st.items); s != st.result {
+			t.Errorf("wanted %s (%v), got %s", st.items,
+				st.result, s)
+		}
 	}
 }
